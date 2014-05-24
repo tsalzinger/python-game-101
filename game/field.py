bd1a836115f68506b56635b65fc551918934e943
Armin <issue@archlinux.info>
2014-05-24 21:16:50 +0200

    added directory structure and entity stubs

--- game/field.py
+++ game/field.py
@@ -3,7 +3,10 @@ from static_entity import StaticEntity
 
 
 class Field(object):
-    path = []
-    size = (None, None)
-    entry_point = (None, None)
-    base = StaticEntity()
+    def __init__(self, path, size, entry_point, base):
+        self.path = path
+        self.size = size
+        self.entry_point = entry_point
+        self.base = base
+
+
