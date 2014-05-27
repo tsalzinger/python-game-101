fd7b269d514f76bbf9ad56cb07c7ad28eb1bcef9
issue <armin@praher.net>
2014-05-27 19:20:37 +0200

    implemented gamefield scaling base and entry_point are drawn, path drawing currently broken because of https://bitbucket.org/marcusva/py-sdl2/issue/56/sdl2extrendererdraw_line-error-when

---
+++ game/field_drawer.py
@@ -0,0 +1,62 @@
+__author__ = 'issue'
+from field import Field
+
+
+SCALE_FACTOR = 60
+
+
+def scale(field):
+    """
+    Scale the game field for drawing
+    """
+
+    scaled_path = [(a * SCALE_FACTOR, b * SCALE_FACTOR) for (a, b) in field.path]
+    scaled_size = tuple([a * SCALE_FACTOR for a in field.size])
+    scaled_entry_point = tuple([a * SCALE_FACTOR for a in field.entry_point])
+    scaled_base = tuple([a * SCALE_FACTOR for a in field.base])
+
+    return Field(scaled_path, scaled_size, scaled_entry_point, scaled_base)
+
+
+def _create_rectangle(coords):
+    return coords[0], coords[1], 30, 30
+
+
+class FieldDrawer(object):
+    def __init__(self, field, renderer):
+        self.scaled_field = scale(field)
+        self.renderer = renderer
+        self.draw()
+
+    def draw(self):
+        """
+        Draws the scaled field on a given SDL2 surface
+        SDL2 renderer.draw_line wants a tuple and start and end points
+        """
+        #print(self._create_rectangle())
+        self.renderer.draw_rect(_create_rectangle(self.scaled_field.base))
+        self.renderer.draw_rect(_create_rectangle(self.scaled_field.entry_point))
+        #self.renderer.draw_line(self._calculate_path()) # fixed in git version of pysdl2
+        self.renderer.present()
+
+
+    def _calculate_path(self):
+        """
+        Path needs to have an start point and end point for every line to draw
+        """
+        new_path = ()
+        for elem in self.scaled_field.path:
+            thiselem = elem
+            try:
+                nextelem = self.scaled_field.path[self.scaled_field.path.index(elem) + 1]
+            except IndexError as e:
+                break
+            new = thiselem + nextelem
+            new_path = new_path + new
+        return new_path
+
+
+
+
+
+
--- run.py
+++ run.py
@@ -1,14 +1,24 @@
 import sys
 import sdl2
 import sdl2.ext
+from field_drawer import FieldDrawer
+from field import Field
 
 __author__ = 'issue'
+
+path = [(0, 0), (5, 5), (7, 7), (10, 10)]  # left upper to lower right
+size = (10, 10)
+test_field = Field(path, size, entry_point=(2, 2), base=(8, 8))
 
 
 def run():
     sdl2.ext.init()
-    window = sdl2.ext.Window("ToDO", size=(640, 480))
+    window = sdl2.ext.Window("ToDO", size=(640, 600))
     window.show()
+    renderer = sdl2.ext.Renderer(window)
+    fd = FieldDrawer(test_field, renderer)
+    fd.draw()
+
     running = True
     while running:
         events = sdl2.ext.get_events()
